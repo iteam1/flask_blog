e0a322be2dd33241cc7dd7249a2d15fe9942162c
LocChuong <locchuong123@gmail.com>
2021-10-15 12:01:54 +0700

    template
    LocChuong

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 from flasgger import Swagger,swag_from
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": 'Loc Chuong',
+        "title": 'Blog Post 1',
+        "content": 'First post content',
+        "date_posted": 'April 21, 2018' 
+    },
+    {
+        "author": 'Loc Chuong',
+        "title": 'Blog Post 2',
+        "content": 'Second post content',
+        "date_posted": 'April 21, 2018' 
+    }
+
+]
 
 #define a template Info Object
 template = {
@@ -24,12 +40,12 @@ swagger = Swagger(app,template = template)
 @app.route("/home")
 @swag_from('./docs/homePage.yml')
 def home():
-    return "<h1>Home Page!</h1>",200
+    return render_template('home.html', posts = posts),200
 
 @app.route("/about")
 @swag_from('./docs/aboutPage.yml')
 def about():
-    return "<h1>About Page</h1>",201
+    return render_template('about.html', title = 'About'),201
 
 if __name__ == "__main__":
     app.run(debug = True)
