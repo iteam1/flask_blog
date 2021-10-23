ea3bd75d59d4c4d309b4e311fd424836e061a02b
LocChuong <locchuong123@gmail.com>
2021-10-23 11:33:20 +0700

    LocChuong
    UserProfile

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed # filefield for upload file, file allowed for avlidate file
 from wtforms import StringField, PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired, Length, Email,EqualTo,ValidationError
 from flaskblog.models import User
+from flask_login import current_user # for account udate class
+
 
 # create a registration form inherited from flask form
 class RegistrationForm(FlaskForm):
@@ -27,3 +30,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators = [DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+# create a update account form
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators= [Length(min= 2,max =20)])
+    email = StringField('Email', validators = [Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    #function to validate username
+    def validate_username(self,username):
+        if username.data != current_user.username: # if there is change in username
+            user = User.query.filter_by(username = username.data).first()
+            if user: # if there is any user
+                raise ValidationError(f'The username {username.data} is already taken, Please try another!')
+
+    def validate_email(self,email):
+        if email.data != current_user.email: # if there is change in email
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError(f'The email {email.data} is already taken, Please try another!')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template,flash, redirect,url_for,request
 from flaskblog import app,db,bcrypt 
-from flaskblog.forms import RegistrationForm,LoginForm
+from flaskblog.forms import RegistrationForm,LoginForm,UpdateAccountForm
 from flaskblog.models import User,Post
 from flasgger import Swagger,swag_from
 from flask_login import login_user,current_user,logout_user,login_required
+import secrets # for generate new name of the picture upload
+import os # for generate new name of the picture upload
+from PIL import Image # for generate new name of the picture upload
 
 posts = [
     {
@@ -96,9 +99,39 @@ def logout():
     flash(f'You are logout!','info')
     return redirect(url_for('home'))
 
-@app.route("/account")
+# this function for generate new name of the upload picture
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name,f_ext = os.path.splitext(form_picture.filename) # split the extesion out the picture name
+    picture_fn = random_hex + f_ext # picture new name 
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn) # create a path
+
+    output_size = (125,125) # follow css style
+    i = Image.open(form_picture) # create a new image follow css style
+    i.thumbnail(output_size)
+
+    i.save(picture_path) # save the new picture into picture path
+    return picture_fn 
+
+@app.route("/account",methods = ['GET','POST'])
 @login_required # need login to access this route
 @swag_from('./docs/account.yml')
 def account():
-    return render_template('account.html',title = 'Account'),206
+    form = UpdateAccountForm()
+    # if there is a post request
+    if form.validate_on_submit():
+        if form.picture.data: # if there is a picture in update file
+            picture_file = save_picture(form.picture.data) # save the new name
+            current_user.image_file = picture_file # update current user ' new picture name
+        current_user.username = form.username.data # current_user use class User
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f'Your account has been updated','success')
+        return redirect(url_for('account'))
+    # if there is a get request, display current_user's info on the form
+    if request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static',filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html',title = 'Account',image_file = image_file,form = form),206
 
